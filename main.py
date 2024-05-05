1757c7fc8e7ddf83cdc65b66a851b9c081b62c25
Edoardo Miglioli <edoardomiglioli2008@gmail.com>
2024-05-05 15:57:31 +0200

    messages function

--- main.py
+++ main.py
@@ -19,19 +19,43 @@ account_sid = os.environ['TWILIO_ACCOUNT_SID']
 auth_token = os.environ['TWILIO_AUTH_TOKEN']
 client = Client(account_sid, auth_token)
 
-message_text = ""
+message_text = (
+"""
+Questo messaggio è riservato ai propretari
+ dei bagni della Riviera. Ti abbiamo allegato un 
+ questionario che ha l'obiettivo di darti la possibilità
+ di condividerci la tua opinione per capire se il nostro
+ prodotto può aiutare il tuo business. Tutte le informazioni sono
+ descritte nel questionario. Grazie mille per il tuo tempo.
+
+ per qualsiasi informazione, scrivi pure a questo numero: 
+
+ https://forms.gle/mw7kP7NdTnPCERRU8
+"""
+)
 
 def get_phone_numbers():
      cur.execute("SELECT tel FROM business;")
      rows = cur.fetchall()
 
-     return [row[0] for row in rows if row[0]]
+     raw_numbers =  [[num.replace(" ", "") for num in row[0] if num[0] != "0"] for row in rows if row[0]] 
+
+     return [tel for tel in raw_numbers if tel]
 
 def send_message(number):
      message = client.messages.create(
-                         body="Hi!",
+                         body={message_text},
                          from_=os.environ['PHONE_NUMBER'],
-                         to={number}
+                         to={"+39" + number}
                     )
 
-print(get_phone_numbers())
+phone_numbers = get_phone_numbers()
+
+messages_sent = 0
+
+for tel_array in phone_numbers:
+     for tel in tel_array:
+          messages_sent += 1
+          send_message(tel)
+
+print(messages_sent)
