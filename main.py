535dafa152eb40fbb38331dafd85d4324faa3215
Edoardo Miglioli <edoardomiglioli2008@gmail.com>
2024-05-05 14:38:16 +0200

    got phone numbers from db

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-import os
-from twilio.rest import Client
 from dotenv import load_dotenv
+import os
+import psycopg2
+from twilio.rest import Client
 
 load_dotenv()
+
+conn = psycopg2.connect(
+    dbname=os.environ.get("DB_NAME"),
+    user=os.environ.get("DB_USER"),
+    password=os.environ.get("DB_PASS"),
+    host=os.environ.get("DB_HOST"),
+    port='5432'
+)
+
+cur = conn.cursor()
 
 account_sid = os.environ['TWILIO_ACCOUNT_SID']
 auth_token = os.environ['TWILIO_AUTH_TOKEN']
@@ -10,10 +21,13 @@ client = Client(account_sid, auth_token)
 
 message_text = ""
 
-message = client.messages.create(
-                     body="Hi!",
-                     from_=os.environ['PHONE_NUMBER'],
-                     to='number'
-                )
+def get_phone_numbers():
+     cur.execute("SELECT tel FROM business;")
+     return cur.fetchall()
 
-print(message)
+def send_message(number):
+     message = client.messages.create(
+                         body="Hi!",
+                         from_=os.environ['PHONE_NUMBER'],
+                         to={number}
+                    )
